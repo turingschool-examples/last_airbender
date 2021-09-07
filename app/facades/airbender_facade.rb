2eb2a5347ecf01934f234b10791959759d74fef5
Scott Borecki <scottborecki@gmail.com>
2021-09-07 11:00:56 -0600

    Add dynamic input for per_page

--- app/facades/airbender_facade.rb
+++ app/facades/airbender_facade.rb
@@ -2,7 +2,7 @@ class AirbenderFacade
   # There are 497 characters in total.  This endpoint request will retrieve
   #   up to 497 characters.
   def self.nation_members(nation)
-    json = AirbenderService.nation_members(nation)
+    json = AirbenderService.nation_members(nation, per_page = 497)
 
     json.map { |member_data| Member.new(member_data) }
   end
--- app/services/airbender_service.rb
+++ app/services/airbender_service.rb
@@ -5,9 +5,9 @@ class AirbenderService
 
   # There are 497 characters in total.  This endpoint request will retrieve
   #   up to 497 characters.
-  def self.nation_members(nation)
+  def self.nation_members(nation, per_page = 497)
     nation = nation.gsub('_', '+')
-    response = conn.get("/api/v1/characters?affiliation=#{nation}&perPage=497")
+    response = conn.get("/api/v1/characters?affiliation=#{nation}&perPage=#{per_page}")
 
     JSON.parse(response.body, symbolize_names: true)
   end

2833ce94d943a65d7dde37679d93e1e476b63630
Matt Kragen <mattdkragen@gmail.com>
2021-09-07 09:54:23 -0700

    Attempt clever tests

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,5 +3,6 @@ class SearchController < ApplicationController
     affiliation = set_affiliation
     response = Faraday.get("https://last-airbender-api.herokuapp.com/api/v1/characters?#{affiliation}&perPage=150")
     @inhabitants = JSON.parse(response.body, symbolize_names: true)
+    @nation = params[:nation]
   end
 end
--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -12,17 +12,23 @@ RSpec.describe "search index" do
   end
 
   describe 'search display' do
-    it 'displays total number of inhabitants' do
+    before :each do
       visit root_path
       select "Fire Nation"
       click_on "Search For Members"
-      
-      expect(current_path).to eq(search_path)
+    end
+
+    it 'displays total number of inhabitants' do
       expect(page).to have_content('Total Inhabitants: 97')
     end
 
     it 'displays first 25 inhabitants and information' do
-      expect(page).to have_content()      
+      within("#first-25") do
+        # h3 tags are only used for names
+        expect(page).to have_css('h3', count: 25)
+        # p tags are for supplementary information in sets of 3
+        expect(page).to have_css('p', count: 75)
+      end
     end
   end
 end

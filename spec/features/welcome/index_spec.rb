e2fa0a6bdb4371fcf1180f9f4ca2b921558c66cb
Scott Borecki <scottborecki@gmail.com>
2021-09-07 10:51:49 -0600

    Add test for displaying 25 members

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -7,8 +7,8 @@ describe 'welcome index page' do
 
       context 'and I select "Fire Nation" from the select field and I click "Search For Members"' do
         let(:nation) { 'Fire Nation' }
-        let(:total_number) { 97 }
-
+        let(:total_member_count) { 97 }
+        let(:fire_nation_members) { AirbenderFacade.nation_members('fire_nation') }
 
         before do
           select nation, from: :nation
@@ -20,7 +20,7 @@ describe 'welcome index page' do
         end
 
         it 'displays the total number of people who live in the Fire Nation' do
-          expect(page).to have_content("Total Number of #{nation} Members: #{total_number}")
+          expect(page).to have_content("Total Number of #{nation} Members: #{total_member_count}")
         end
 
         it 'lists up to the first 25 members of the Fire Nation' do
@@ -28,13 +28,19 @@ describe 'welcome index page' do
         end
 
         it 'displays the details of each member' do
-          fire_nation_members = AirbenderFacade.nation_members('fire_nation')[0..24]
-
-          expect(fire_nation_members.size).to eq(25)
+          expect(fire_nation_members[0..24].size).to eq(25)
 
           fire_nation_members[0..24].each do |member|
+            expect(page).to have_css("#member-#{member.id}")
+
             within "#member-#{member.id}" do
               expect(page).to have_content(member.name)
+
+              # NOTE: Guard clause used because not all members have a photo URL
+              #       When a photo URL is not available, it will default to nil
+              unless member.photo_url.nil?
+                expect(page).to have_css("#image-#{member.id}")
+              end
 
               if member.allies.empty?
                 expect(page).to have_content('None')
@@ -50,6 +56,12 @@ describe 'welcome index page' do
 
               expect(page).to have_content(member.affiliation)
             end
+          end
+        end
+
+        it 'does not display members after the first 25' do
+          fire_nation_members[25..].each do |member_after_25|
+            expect(page).to have_no_css("#member-#{member_after_25.id}")
           end
         end
       end

b48da43d64f3c7f74f2b0e458d3de8a89a3a398e
Ezzedine Alwafai <ezzowafai@gmail.com>
2021-09-07 11:57:12 -0500

    Update params

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,6 @@
 class SearchController < ApplicationController
   def index
-    results = MemberFacade.member_search(params[:nation])
+    results = MemberFacade.member_search(params[:affiliation])
     @members = results[:top_twenty_five_results]
   end
 end

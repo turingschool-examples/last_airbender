e5da228a2d0d8b32ae634a7768b87f5636020efe
Antonio King <antonio.j.king@gmail.com>
2021-09-07 09:31:00 -0600

    Create features spec for avatar search page; incomplete

---
+++ spec/features/avatars/search_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe 'Avatar Kingdom Show Page' do
+  # As a user,
+  # When I visit "/"
+  # And I Select "Fire Nation" from the select field
+  # (Note: Use the existing select field)
+  # And I click "Search For Members"
+  # Then I should be on page "/search"
+  # Then I should see the total number of people who live in the Fire Nation. (should be close to 100)
+  # And I should see a list with the detailed information for the first 25 members of the Fire Nation.
+  #
+  # And for each of the members I should see:
+  # - The name of the member (and their photo, if they have one)
+  # - The list of allies or "None"
+  # - The list of enemies or "None"
+  # - Any affiliations that the member has
+  it 'displays list of members associated with avatar kingdom' do
+    visit "/"
+
+    select "Fire Nation", from: :nation
+    click_button "Submit"
+
+    expect(current_path).to eq("/search")
+    expect(page).to have_content()
+  end
+end

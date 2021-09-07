c62a98e22ff140257ecc1050e562ebf5db2d8e93
Scott Borecki <scottborecki@gmail.com>
2021-09-07 10:24:09 -0600

    Search feature development in progress

---
+++ app/controllers/search_controller.rb
@@ -0,0 +1,6 @@
+class SearchController < ApplicationController
+  def index
+    @nation = params[:nation]
+    @members = AirbenderFacade.nation_members(params[:nation])
+  end
+end
--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -6,25 +6,27 @@ describe 'welcome index page' do
       before { visit root_path }
 
       context 'and I select "Fire Nation" from the select field and I click "Search For Members"' do
+        let(:nation) { 'Fire Nation' }
+        let(:total_number) { 20 }
+
         before do
-          select 'Fire Nation', from: :nation
+          select nation, from: :nation
           click_button 'Search For Members'
         end
 
         it 'redirects me to the "/search" page' do
-          expect(page).to have_current_path('/search')
+          expect(current_path).to eq('/search')
         end
 
         it 'displays the total number of people who live in the Fire Nation' do
-          total_number = 100 # or so
-          expect(page).to have_content("Total Number of People in the Fire Nation: #{total_number}")
+          expect(page).to have_content("Total Number of #{nation} Members: #{total_number}")
         end
 
-        it 'lists the first 25 members of the Fire Nation' do
-          expect(page).to have_content('First 25 Members of the Fire Nation')
+        it 'lists up to the first 25 members of the Fire Nation' do
+          expect(page).to have_content('Members of the Fire Nation')
         end
 
-        it 'displays the details of each member' do
+        xit 'displays the details of each member' do
           members.each do |member|
             expect(page).to have_content(member.name)
             expect(page).to have_content(member.allies) # or "None"

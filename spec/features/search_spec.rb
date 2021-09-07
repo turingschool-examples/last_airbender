62c082b07771fdbf0957b9b5f33c46f3cc106c57
JoannaCoPo <jococopo@gmail.com>
2021-09-07 09:22:40 -0600

    Intial feature test setup.

---
+++ spec/features/search_spec.rb
@@ -0,0 +1,30 @@
+require 'rails_helper'
+
+RSpec.describe 'Search index' do
+  describe 'search for members of a specific nation' do
+    it 'displays the total number of people who live in nation' do
+      visit root_path
+
+      find("nation").find(:xpath, 'option[3]').select_option
+      click_on "Search For Members"
+
+      expect(current_path).to eq(search_path)
+      expect(page).to have_content('the total number of people who live in the Fire Nation')
+    end
+  end
+end
+
+# As a user,
+# When I visit "/"
+# And I Select "Fire Nation" from the select field
+# (Note: Use the existing select field)
+# And I click "Search For Members"
+# Then I should be on page "/search"
+# Then I should see the total number of people who live in the Fire Nation. (should be close to 100)
+# And I should see a list with the detailed information for the first 25 members of the Fire Nation.
+#
+# And for each of the members I should see:
+# - The name of the member (and their photo, if they have one)
+# - The list of allies or "None"
+# - The list of enemies or "None"
+# - Any affiliations that the member has

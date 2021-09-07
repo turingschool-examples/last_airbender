cac01a7d923fa66c4ad1774fe96ce76838171c40
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-07 12:31:43 -0400

    Update character poro to have affiliation attribute

--- app/poros/character.rb
+++ app/poros/character.rb
@@ -1,10 +1,11 @@
 class Character
-  attr_reader :name, :allies, :enemies, :image
+  attr_reader :name, :allies, :enemies, :image, :affiliation
 
   def initialize(char)
     @name = char[:name]
     @allies = char[:allies]
     @enemies = char[:enemies]
     @image = char[:photoUrl]
+    @affiliation = char[:affiliation]
   end
 end

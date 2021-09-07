816ab31749abe1a5865ec9ca8f8e3997271423f3
Marla Schulz <marlaa.schulz@gmail.com>
2021-09-07 09:05:00 -0700

    Add to test

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -9,7 +9,28 @@ RSpec.describe 'Index' do
     expect(page).to have_content('Total People: 76')
   end
 
-  xit 'displaysinformation for first 25 members' do
+  it 'displaysinformation for first 25 members' do
+    expect(page).to have_content('Cabbage merchant')
+    expect(page).to have_content('Affiliation: Cabbage Corp Earth Kingdom')
+    within(:css, "#5cf5679a915ecad153ab68f7.allies") do
+      expect(page).to have_content('Allies:')
+      expect(page).to have_content('Cabbage Corp')
+    end
 
+    within(:css, '#5cf5679a915ecad153ab68f7.enemies') do
+      expect(page).to have_content('Enemies:')
+      expect(page).to have_content('The')
+    end
+
+    expect(page).to have_content('Gun')
+    expect(page).to have_content('Affiliation: Earth Kingdom government')
+    within(:css, "#5cf5679a915ecad153ab694f.allies") do
+      expect(page).to have_content('Allies:')
+      expect(page).to have_content('Earth Queen')
+    end
+    within(:css, '#5cf5679a915ecad153ab694f.enemies') do
+      expect(page).to have_content('Enemies:')
+      expect(page).to have_content('None')
+    end
   end
 end

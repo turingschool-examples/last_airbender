ce7b28a134c44a85dce0c851b2dde1395320d2cd
Caroline Tan <carolinectan3@gmail.com>
2021-09-07 12:21:35 -0400

    Build out search index view

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -18,7 +18,7 @@ RSpec.describe 'welcome index page' do
     # And for each of the members I should see:
     within '#character-id-5cf5679a915ecad153ab68fd' do
       # - The name of the member (and their photo, if they have one)
-      expect(page).to have_content('Name: Chan (Fire Nation admiral)')
+      expect(page).to have_content('Chan (Fire Nation admiral)')
       # - The list of allies or "None"
       expect(page).to have_content('Allies: Ozai')
       # - The list of enemies or "None"

661848cfc33378d6d79e098bd6472752e510a152
Lee Hopper <hopper.lee.j@gmail.com>
2021-09-07 10:49:11 -0600

    Refactor params to private method

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,11 +1,15 @@
 class SearchController < ApplicationController
   def index
-    @nation = convert_nation_input(params[:nation])
+    @nation = convert_nation_input(nation_params[:nation])
     @people = SearchFacade.nation_search(@nation)
     @member_count = SearchFacade.nation_member_count(@nation)
   end
 
   private
+  def nation_params
+    params.permit(:nation)
+  end
+
   def convert_nation_input(nation)
     nation.split("_").join(" ")
   end

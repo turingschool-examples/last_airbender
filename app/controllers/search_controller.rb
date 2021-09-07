c626389797ac526372cdb0954cc4fb8a3455434d
Jacob Piland <piland.jacob@gmail.com>
2021-09-07 11:17:22 -0500

    Add processing to manipulate params_nation

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,4 +1,9 @@
 class SearchController < ApplicationController
   def index
+    split = params[:nation].split('_')
+    first = split.first
+    last = split.last
+    params_nation = first + '+' + last
+    @characters = CharacterFacade.characters_fetch(params_nation)
   end
 end

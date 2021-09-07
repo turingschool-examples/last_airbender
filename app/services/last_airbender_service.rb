bf9602f700ab2ada959612edcf7d111723b12afc
Matt Toensing <matthew.toensing@gmail.com>
2021-09-07 09:07:51 -0700

    Complete service with all members of a nation present

--- app/services/last_airbender_service.rb
+++ app/services/last_airbender_service.rb
@@ -3,10 +3,21 @@ class LastAirbenderService
     Faraday.new(url)
   end
 
-  def self.get_affiliations(query)
+  def self.get_affiliations(query, page)
     response = conn('https://last-airbender-api.herokuapp.com/api/v1').get('/characters') do |f|
       f.params['affiliation'] = query
+      f.params['page'] = page
     end
     JSON.parse(response.body, symbolize_names: true)
+  end
+
+  def self.get_all_affiliations(query)
+    page = 1
+    results = []
+    until get_affiliations(query, page).empty?
+      results << get_affiliations(query, page)
+      page += 1
+    end
+    results.flatten
   end
 end
--- spec/services/last_airbender_service_spec.rb
+++ spec/services/last_airbender_service_spec.rb
@@ -8,19 +8,18 @@ require 'rails_helper'
    it 'can get nation information' do
      json_response = File.read('spec/fixtures/characters.json')
 
-     stub_request(:get, "https://last-airbender-api.herokuapp.com/characters?affiliation=Fire%20Nation").
-    with(
-      headers: {
-     'Accept'=>'*/*',
-     'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-     'User-Agent'=>'Faraday v1.7.1'
-      }).
-    to_return(status: 200, body: json_response, headers: {})
+     stub_request(:get, "https://last-airbender-api.herokuapp.com/characters?affiliation=Fire%20Nation&page=1").
+            with(
+              headers: {
+          	  'Accept'=>'*/*',
+          	  'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          	  'User-Agent'=>'Faraday v1.7.1'
+              }).
+            to_return(status: 200, body: json_response, headers: {})
 
-     search = 'Fire Nation'
-     json = LastAirbenderService.get_affiliations(search)
-
-     expect(json.count).to eq(20)
+      search = 'Fire Nation'
+      page = 1
+      json = LastAirbenderService.get_affiliations(search, page)
 
      expect(json).to be_a(Array)
      json.each do |member|
@@ -32,15 +31,77 @@ require 'rails_helper'
    end
 
    it 'can combine pages to get all members of a nation' do
-     json_response = File.read('spec/fixtures/characters.json')
+     json_response1 = File.read('spec/fixtures/characters.json')
 
-     stub_request(:get, "https://last-airbender-api.herokuapp.com/characters?affiliation=Fire%20Nation").
-    with(
-      headers: {
-     'Accept'=>'*/*',
-     'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
-     'User-Agent'=>'Faraday v1.7.1'
-      }).
-    to_return(status: 200, body: json_response, headers: {})
+
+     stub_request(:get, "https://last-airbender-api.herokuapp.com/characters?affiliation=Fire%20Nation&page=1").
+     with(
+       headers: {
+      'Accept'=>'*/*',
+      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'User-Agent'=>'Faraday v1.7.1'
+       }).
+     to_return(status: 200, body: json_response1, headers: {})
+
+     json_response2 = File.read('spec/fixtures/page2.json')
+
+     stub_request(:get, "https://last-airbender-api.herokuapp.com/characters?affiliation=Fire%20Nation&page=2").
+     with(
+       headers: {
+      'Accept'=>'*/*',
+      'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+      'User-Agent'=>'Faraday v1.7.1'
+       }).
+     to_return(status: 200, body: json_response2, headers: {})
+
+     json_response3 = File.read('spec/fixtures/page3.json')
+
+     stub_request(:get, "https://last-airbender-api.herokuapp.com/characters?affiliation=Fire%20Nation&page=3").
+       with(
+         headers: {
+        'Accept'=>'*/*',
+        'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+        'User-Agent'=>'Faraday v1.7.1'
+         }).
+       to_return(status: 200, body: json_response3, headers: {})
+
+       json_response4 = File.read('spec/fixtures/page4.json')
+
+       stub_request(:get, "https://last-airbender-api.herokuapp.com/characters?affiliation=Fire%20Nation&page=4").
+         with(
+           headers: {
+          'Accept'=>'*/*',
+          'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+          'User-Agent'=>'Faraday v1.7.1'
+           }).
+         to_return(status: 200, body: json_response4, headers: {})
+
+      json_response5 = File.read('spec/fixtures/page5.json')
+
+      stub_request(:get, "https://last-airbender-api.herokuapp.com/characters?affiliation=Fire%20Nation&page=5").
+           with(
+             headers: {
+            'Accept'=>'*/*',
+            'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+            'User-Agent'=>'Faraday v1.7.1'
+             }).
+           to_return(status: 200, body: json_response5, headers: {})
+
+    json_response6 = File.read('spec/fixtures/page6.json')
+
+    stub_request(:get, "https://last-airbender-api.herokuapp.com/characters?affiliation=Fire%20Nation&page=6").
+                with(
+                  headers: {
+                 'Accept'=>'*/*',
+                 'Accept-Encoding'=>'gzip;q=1.0,deflate;q=0.6,identity;q=0.3',
+                 'User-Agent'=>'Faraday v1.7.1'
+                  }).
+                to_return(status: 200, body: json_response6, headers: {})
+
+
+    search = 'Fire Nation'
+    json = LastAirbenderService.get_all_affiliations(search)
+
+    expect(json.count).to eq(97)
    end
  end

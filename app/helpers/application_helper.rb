975cddd5e5f5061dcd92397342902410df7b08d9
Jacob Martinez <jacobmarcareer@gmail.com>
2021-09-07 10:52:11 -0600

    Make results more general

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
     def element_nation
         [
-          ['Air Nomads', 'air+nomads'],
+          ['Air Nomads', 'air+nomad'],
           ['Earth Kingdom', 'earth+kingdom'],
           ['Fire Nation', 'fire+nation'],
-          ['Water Tribes', 'water+tribes'],
+          ['Water Tribes', 'water+tribe'],
         ]
       end
 end

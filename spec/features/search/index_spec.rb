990baa290cb8fa5f73c2f1cdbd9d17fa942b36b5
Lee Hopper <hopper.lee.j@gmail.com>
2021-09-07 10:56:18 -0600

    Refactor display testing to better examples of members and with ids for enemies and allies

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -16,23 +16,42 @@ RSpec.describe 'the search index' do
 
       within('#5cf5679a915ecad153ab68fd') do
         expect(page).to have_content('Chan')
-        expect(page).to have_content('Ozai')
-        expect(page).to have_content('Earth Kingdom')
+        within('#allies') do
+          expect(page).to have_content('Ozai')
+        end
+        within('#enemies') do
+          expect(page).to have_content('Earth Kingdom')
+        end
         expect(page).to have_content('Affiliation: Fire Nation Navy')
       end
 
       within('#5cf5679a915ecad153ab691c') do
         expect(page).to have_content('Elua')
-        expect(page).to have_content('Ozai')
-        expect(page).to have_content('None')
+        within('#allies') do
+          expect(page).to have_content('Ozai')
+        end
+        within('#enemies') do
+          expect(page).to have_content('None')
+        end
         expect(page).to have_content('Affiliation: Fire Nation Fire Nation Royal Family')
       end
 
-      within('#5cf5679a915ecad153ab692a') do
-        expect(page).to have_content('Fire Nation train conductor')
-        expect(page).to have_content('Aang')
-        expect(page).to have_content('Combustion Man')
-        expect(page).to have_content('Affiliation: Fire Nation Fire Nation train')
+      within('#5cf5679a915ecad153ab68da') do
+        expect(page).to have_content('Azula')
+        within('#allies') do
+          expect(page).to have_content('Ozai')
+          expect(page).to have_content('Zuko')
+        end
+        within('#enemies') do
+          expect(page).to have_content('Iroh')
+          expect(page).to have_content('Zuko')
+          expect(page).to have_content('Kuei')
+          expect(page).to have_content('Long Feng')
+          expect(page).to have_content('Mai')
+          expect(page).to have_content('Ty Lee')
+          expect(page).to have_content('Ursa')
+        end
+        expect(page).to have_content("Azula's team (formerly) Dai Li (formerly) Fire Nation Fire Nation Royal Family Fire Warriors Royal Fire Academy for Girls (formerly)")
       end
     end
   end

da290b93efa498ce95a5c783bbd26a2ade45f6f2
Matt Kragen <mattdkragen@gmail.com>
2021-09-07 09:39:12 -0700

    Continue through testing

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -20,16 +20,12 @@ RSpec.describe "search index" do
       expect(current_path).to eq(search_path)
       expect(page).to have_content('Total Inhabitants: 97')
     end
+
+    it 'displays first 25 inhabitants and information' do
+      expect(page).to have_content()      
+    end
   end
 end
-
-# As a user,
-# When I visit "/"
-# And I Select "Fire Nation" from the select field
-# (Note: Use the existing select field)
-# And I click "Search For Members"
-# Then I should be on page "/search"
-# Then I should see the total number of people who live in the Fire Nation. (should be close to 100)
 # And I should see a list with the detailed information for the first 25 members of the Fire Nation.
 # 
 # And for each of the members I should see:

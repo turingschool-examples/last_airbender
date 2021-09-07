3f9283bea71af513710f7d2e6ee4b7a645d54e5f
sami-p <sami.peterson14@gmail.com>
2021-09-07 15:16:05 -0500

    Refactor api to correct per page, test and fix view

---
+++ spec/features/search/index_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'Search Index Page' do
+  it 'it displays nation name' do
+    visit root_path
+
+    select 'Fire Nation', :from => 'nation'
+
+    click_on "Search For Members"
+
+    expect(current_path).to eq(search_path)
+    expect(page).to have_content("Fire Nation")
+    expect(page).to have_content("Chan (Fire Nation admiral)")
+    expect(page).to have_content("Fire Nation bar patron's friend")
+    expect(page).to have_content("25")
+  end
+end

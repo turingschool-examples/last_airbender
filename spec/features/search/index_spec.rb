01c8cfab5a963a3779a33500b3934d5529da3a54
Michael Abbott <michael@mjabbottdesign.com>
2021-09-07 08:59:53 -0700

    Add search feature index test

---
+++ spec/features/search/index_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'The search index page' do
+  before :each do
+    visit root_path
+
+    select 'Fire Nation'
+    click_on 'Search For Members'
+  end
+
+  it 'displays the total number of people who live in the selected nation' do
+    expect(current_path).to eq(search_path)
+    expect(page).to have_content('Total number of people: 97')
+  end
+end

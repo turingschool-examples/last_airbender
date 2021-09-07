bae6ff7d98f2881d795dfb0bd80acb98411ccf30
Kim Abcouwer <kimberly.abcouwer@gmail.com>
2021-09-07 10:31:03 -0600

    Complete facades services and poros

--- app/facades/last_airbender_facade.rb
+++ app/facades/last_airbender_facade.rb
@@ -1,12 +1,17 @@
 class LastAirbenderFacade
-  def self.total_characters(nation, pages)
+  def self.total_characters(nation, pages = 5)
     response = (1..pages).sum do |i|
       LastAirbenderService.characters(nation, i)
     end
+    create_character_objects(response)
   end
 
   def self.top_characters(nation)
     response = total_characters(nation, 2)
     response[0..24]
+  end
+
+  def self.create_character_objects(response)
+    response.map { |data| Character.new(data) }
   end
 end
--- spec/poros/character_spec.rb
+++ spec/poros/character_spec.rb
@@ -3,27 +3,20 @@ require 'rails_helper'
 RSpec.describe Character do
   it 'can create character objects' do
     nation = 'Fire+Nation'
-    response = LastAirbenderFacade.top_characters(nation)
+    response = LastAirbenderFacade.total_characters(nation)
 
-    response.each do |character|
-      new_character = Character.new(character)
-
-      expect(new_character.id).to be_a(String)
-      expect(new_character.name).to be_a(String)
-      expect(new_character.allies).to be_an(Array)
-      expect(new_character.enemies).to be_an(Array)
-      expect(new_character.affiliation).to be_a(String)
-    end
+    expect(response.first.id).to be_a(String)
+    expect(response.first.name).to be_a(String)
+    expect(response.first.allies).to be_an(Array)
+    expect(response.first.enemies).to be_an(Array)
+    expect(response.first.affiliation).to be_a(String)
   end
 
   it 'has a photo or nil' do
     nation = 'Fire+Nation'
     response = LastAirbenderFacade.top_characters(nation)
 
-    character1 = Character.new(response.first)
-    character2 = Character.new(response[1])
-
-    expect(character1.photo).to eq(nil)
-    expect(character2.photo).to be_a(String)
+    expect(response.first.photo).to eq(nil)
+    expect(response[1].photo).to be_a(String)
   end
 end

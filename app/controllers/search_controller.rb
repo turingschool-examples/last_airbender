895a544c6d9a313ffd16438e90017a5d84480db7
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-07 12:37:08 -0400

    Clean up controller

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,8 +1,8 @@
 class SearchController < ApplicationController
   def index
-    if params[:nation] && params[:nation].include?("fire")
-      @total_chars = SearchFacade.chars_from_fire_nation.count
-      @chars25 = SearchFacade.twenty_five_in_detail
+    if params[:nation]
+      @total_chars = SearchFacade.chars_from_fire_nation.count if params[:nation].include?("fire")
+      @chars25 = SearchFacade.twenty_five_in_detail if params[:nation].include?("fire")
     end
   end
 end

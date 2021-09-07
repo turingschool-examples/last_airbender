ce128cbd35026f500d43fe29c10009ab02a9c458
Brian Fletcher <brianpatrickfletcher@gmail.com>
2021-09-07 12:19:24 -0400

    Create instance vars in controlle for view for 25 characters

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -2,6 +2,7 @@ class SearchController < ApplicationController
   def index
     if params[:nation] && params[:nation].include?("fire")
       @chars = SearchFacade.chars_from_fire_nation
+      @chars25 = SearchFacade.twenty_five_in_detail
     end
   end
 end

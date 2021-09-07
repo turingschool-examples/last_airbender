41ce0bfabdf252d19448726c0f4c0d8d74247474
Matt Kragen <mattdkragen@gmail.com>
2021-09-07 10:01:10 -0700

    Finish up work; remove notes

--- spec/features/search/index_spec.rb
+++ spec/features/search/index_spec.rb
@@ -28,14 +28,8 @@ RSpec.describe 'search index' do
         expect(page).to have_css('h3', count: 25)
         # p tags are for supplementary information in sets of 3
         expect(page).to have_css('p', count: 75)
+        save_and_open_page
       end
     end
   end
 end
-# And I should see a list with the detailed information for the first 25 members of the Fire Nation.
-#
-# And for each of the members I should see:
-# - The name of the member (and their photo, if they have one)
-# - The list of allies or "None"
-# - The list of enemies or "None"
-# - Any affiliations that the member has
